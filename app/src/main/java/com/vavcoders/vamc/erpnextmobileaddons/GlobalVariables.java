e8d8be13d9338dfea1eead861ecc646907d664c8
VamC <vwithv1602@gmail.com>
2018-03-21 19:25:17 +0530

    Alternate way for #24 - https://github.com/vwithv1602/erpnext_mobile_addons/issues/25

--- app/src/main/java/com/vavcoders/vamc/erpnextmobileaddons/GlobalVariables.java
+++ app/src/main/java/com/vavcoders/vamc/erpnextmobileaddons/GlobalVariables.java
@@ -97,7 +97,7 @@ public class GlobalVariables extends DatabaseHelper{
             }
 
         }
-        
+
         this.deleteLeadsQueue();
 
 
--- app/src/main/java/com/vavcoders/vamc/erpnextmobileaddons/LeadSyncFragment.java
+++ app/src/main/java/com/vavcoders/vamc/erpnextmobileaddons/LeadSyncFragment.java
@@ -2,15 +2,24 @@ package com.vavcoders.vamc.erpnextmobileaddons;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.vavcoders.vamc.helper.DatabaseHelper;
 import com.vavcoders.vamc.model.Settings;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 
 import static com.vavcoders.vamc.model.Settings.LEAD_SYNC;
 
@@ -20,9 +29,10 @@ import static com.vavcoders.vamc.model.Settings.LEAD_SYNC;
 
 public class LeadSyncFragment extends Fragment {
     View myView;
-    Button btn_sync_toggle;
+    Button btn_sync_toggle,btn_force_sync;
     DatabaseHelper db;
     String updated_value;
+    EditText et_force_datetime;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -54,7 +64,69 @@ public class LeadSyncFragment extends Fragment {
                 ft.commit();
             }
         });
+        Calendar calander = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+        String currentDateTimeString = simpleDateFormat.format(calander.getTime());
+        et_force_datetime = myView.findViewById(R.id.et_force_datetime);
+        et_force_datetime.setText(currentDateTimeString.toString());
+        btn_force_sync = myView.findViewById(R.id.btnForceSync);
+        btn_force_sync.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View view) {
+                // clear the database queue table
+                DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
+                dbHelper.deleteLeadsQueue();
+                // upload all calls greater than the given date to lead list
+                String forceDateTimeFrom = et_force_datetime.getText().toString();
+                if(forceDateTimeFrom.isEmpty()){
+                    Calendar calander = Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String currentDateTimeString = simpleDateFormat.format(calander.getTime());
+                    et_force_datetime.setText(currentDateTimeString.toString());
+                }
+                try {
+                    Long millis = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(forceDateTimeFrom).getTime();
+                    HashMap<String, Date> callsForForcingLeads = getCallDetails(millis);
+                    GlobalVariables gv = new GlobalVariables(getActivity());
+                    gv.exportCallLog(callsForForcingLeads);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
         return myView;
+    }
+    private HashMap<String, Date> getCallDetails(Long greaterThan) {
+
+        Cursor managedCursor = getActivity().managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, null);
+        int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
+        HashMap<String, Date> hmIncomingCalls = new HashMap<String, Date>();
+        while (managedCursor.moveToNext()) {
+            String phNumber = managedCursor.getString(number);
+            String callType = managedCursor.getString(type);
+            String callDate = managedCursor.getString(date);
+            Date callDayTime = new Date(Long.valueOf(callDate));
+            String dir = null;
+            int dircode = Integer.parseInt(callType);
+            switch (dircode) {
+                case CallLog.Calls.OUTGOING_TYPE:
+                    dir = "OUTGOING";
+                    break;
+                case CallLog.Calls.INCOMING_TYPE:
+                    dir = "INCOMING";
+                    break;
+                case CallLog.Calls.MISSED_TYPE:
+                    dir = "MISSED";
+                    break;
+            }
+            if ((dir == "INCOMING" || dir == "MISSED") && Long.parseLong(callDate)>greaterThan) {
+                hmIncomingCalls.put(phNumber, callDayTime);
+            }
+        }
+        return hmIncomingCalls;
     }
 }
